223f55873d21c06350ad6cf91c081905ac676499
k3csk3 <szilagyi.viktor89@gmail.com>
2022-08-10 22:22:55 +0200

    kb 90%-ig van kész

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const btnModal = document.querySelector(".btn__modal");
+const modalShow = document.querySelector(".modal__background")
+const modalHide = document.querySelector(".modal__window__X")
+const modalHide2 = document.querySelector(".btn__danger")
+
+btnModal.addEventListener('click', function () {
+    modalShow.classList.add('modal__background--active');
+});
+
+modalHide.addEventListener('click', function() {
+    modalShow.classList.remove('modal__background--active');
+})
+
+modalHide2.addEventListener('click', function() {
+    modalShow.classList.remove('modal__background--active');
+})
+
+
+/*const btnModal = document.querySelector(".btn__modal");
+const modalShow = document.querySelector(".modal__background")
+const modalHide = document.querySelectorAll(".modal__window__X, .btn__danger")
+
+btnModal.addEventListener('click', function () {
+    modalShow.classList.add('modal__background--active');
+});
+
+modalHide.addEventListener('click', function() {
+    modalShow.classList.remove('modal__background--active');
+})*/
+
+
+
+//Ami még hiányzik: ha kikattintok akkor még nem tűnik el, illetve, illetve tabra nem vált elemet.
